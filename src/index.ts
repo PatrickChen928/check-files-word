55892d8833700960ff5ac14ca7543364f587f6ae
cyly <786156072@qq.com>
2022-07-14 11:30:14 +0800

    refactor: init

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import cac from 'cac'
+import { version } from '../package.json'
 import { start } from './start'
 
 const cli = cac()
 
 cli
   .command('[...files]', 'check files')
-  .option('-i, --ignore <ignore>', 'ignore files')
-  .option('-f, --filter <filter>', 'filtters')
+  // .option('-i, --ignore <ignore>', 'ignore files')
+  // .option('-f, --filter <filter>', 'filtters')
   .action(async (files: string[], options: any) => {
     start(files, options.i, options.f);
   })
+  .version(version)
 
 cli.help()
 
